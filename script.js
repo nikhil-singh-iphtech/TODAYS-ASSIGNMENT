4c9c988fada1726b9bad25b05d41151f69d1a242
nikhil-singh-iphtech <nikhil.singh@iphtechnologies.com>
2026-02-19 06:50:39 +0000

    Adding website

---
+++ script.js
@@ -0,0 +1,32 @@
+function welcomeUser() {
+  alert("Welcome to NovaTech!");
+  document.body.style.backgroundColor = "#eef6ff";
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("formMessage");
+
+  if (name === "" || email === "") {
+    message.style.color = "red";
+    message.textContent = "Please fill all fields.";
+  } else {
+    message.style.color = "green";
+    message.textContent = "Thank you! We'll contact you soon.";
+  }
+});
+
+let count = 0;
+
+function increase() {
+  count++;
+  document.getElementById("counter").textContent = count;
+}
+
+function decrease() {
+  count--;
+  document.getElementById("counter").textContent = count;
+}
